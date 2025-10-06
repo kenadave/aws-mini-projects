990d9b642480f04f6cbfe2a2af88ebd5f7829847
Kena <kenadave11@gmail.com>
2025-10-06 16:41:00 +0530

    Create lambda_function.py

---
+++ aws-apigateway-lambda-dynamodb-vs-ec2/getUsersFunction/lambda_function.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
+
+def lambda_handler(event, context):
+    try:
+        response = table.scan()
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps(response['Items'])
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
